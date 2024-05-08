6f9c8430c73b9d3e5960b687ac11017ef1387796
Shreyas K N <shreyas2499@gmail.com>
2024-05-07 23:54:01 -0400

    Added delay statistics

--- APIs.py
+++ APIs.py
@@ -45,6 +45,68 @@ DELAYS_MODEL_PATH = "delays_lr_model"
 PRICES_PIPELINE_PATH = "pipeline_model2"
 DELAYS_PIPELINE_PATH = "delays_pipeline"
 
+
+CARRIER_DELAY = {'UA': 3.423222419985118,
+ 'NK': 3.2118444457475714,
+ 'AA': 4.7917388244190064,
+ 'EV': 5.342242695384107,
+ 'B6': 7.299943174643263,
+ 'PT': 2.539269115342735,
+ 'DL': 3.7599815035051902,
+ 'OO': 6.209415580208714,
+ 'F9': 5.000508332704653,
+ 'YV': 5.67237268244944,
+ 'MQ': 2.6363881908638844,
+ 'C5': 6.434559493742045,
+ 'OH': 3.49215333495421,
+ 'EM': 2.1525951715607676,
+ 'HA': 3.0608543863527027,
+ 'G4': 5.794402889991456,
+ 'ZW': 4.268656961022644,
+ 'YX': 3.003557364412194,
+ 'AS': 2.5637682170002094,
+ 'QX': 2.2097902553837927,
+ 'G7': 4.747592106149391,
+ 'WN': 3.1851960736430476,
+ '9E': 3.1854992326419103,
+ 'CP': 4.858521951123864,
+ 'AX': 6.546503974809003,
+ 'KS': 8.625425170068027,
+ '9K': 2.388185654008439,
+ 'VX': 3.0799443058536866
+}
+
+
+LATE_AIRCRAFT_DELAY = {'UA': 5.1743180972118035,
+ 'NK': 3.694704255360738,
+ 'AA': 4.874551629303676,
+ 'EV': 5.129514361667639,
+ 'B6': 7.2672119548665535,
+ 'PT': 4.982086420736728,
+ 'DL': 2.304132494997973,
+ 'OO': 4.003186489827333,
+ 'F9': 7.564506162860458,
+ 'YV': 6.064572802154132,
+ 'MQ': 4.095667314583926,
+ 'C5': 10.02854928666937,
+ 'OH': 5.852045276494583,
+ 'EM': 6.494479820422374,
+ 'HA': 1.3495462169577033,
+ 'G4': 7.319008289238979,
+ 'ZW': 7.082420583977493,
+ 'YX': 4.000902014364639,
+ 'AS': 2.7092320461748516,
+ 'QX': 2.5150772280068066,
+ 'G7': 7.782649693540782,
+ 'WN': 4.315058973990045,
+ '9E': 3.5869364059012443,
+ 'CP': 6.185060033072111,
+ 'AX': 10.59635040264299,
+ 'KS': 6.386479591836735,
+ '9K': 1.5123568414707655,
+ 'VX': 2.859430295295005}
+
+
 def getCityNamesFromJSON():
     with open('distanceMapping.json', 'r') as file:
         data = json.load(file)
@@ -163,14 +225,18 @@ def predict_delays(data):
         "Dest":"SLC",
         "arrDaypart":"night",
         "depDaypart": "night",
-        "CarrierDelay":0.0,
-        "WeatherDelay":0.0,
-        "NASDelay":0.0,
-        "SecurityDelay":0.0,
-        "LateAircraftDelay":0.0,
+        "CarrierDelay":0.0,          # @karthik -> You can use CARRIER_DELAY["DL"] (DL is Operating_Airline)
+        "WeatherDelay":0.0,          # @karthik -> You can use WEATHER_DELAY["Origin_Dest"] (This weather delay needs to be imported from weatherDelay.json)
+        "NASDelay":0.0,              # @karthik -> You can use NAS_DELAY["Origin_Dest"] (This NAS delay needs to be imported from nasDelay.json)
+        "SecurityDelay":0.0,         # @karthik -> You can use SECURITY_DELAY["Origin"] (This Security delay needs to be imported from securityDelay.json)   
+        "LateAircraftDelay":0.0,     # @karthik -> You can use LATE_AIRCRAFT_DELAY["DL"] (DL is Operating_Airline)
         "Distance":368.0,
         "Operating_Airline": "DL"
     }
+
+
+
+
 
     try:
         data_df_2 = spark.createDataFrame(pd.DataFrame([data_2]))
